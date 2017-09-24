e8f5d3132c3d93f54b2cee079d944ddbf3f4f21b
yevhen <evgenii.chmyhun@gmail.com>
2017-09-24 22:37:36 +0300

    implement exercises 2.66-2.72

--- book_exercises/2_3.rkt
+++ book_exercises/2_3.rkt
@@ -432,3 +432,181 @@
 ;; transform the tree to a list for a better view
 (tree->list-1 (intersection-set4 tree1 '(9 (7 () ()) ())))
 ;; => (7 9)
+
+;; Exercise 2.66
+
+(define (key employee) (car employee))
+
+(define (value employee) (cdr employee))
+
+(define (lookup given-key tree-of-records)
+  (cond ((null? tree-of-records) false)
+        ((equal? given-key (key (entry tree-of-records)))
+         (value (entry tree-of-records)))
+        ((< given-key (key (entry tree-of-records)))
+         (lookup given-key (left-branch tree-of-records)))
+        ((> given-key (key (entry tree-of-records)))
+         (lookup given-key (right-branch tree-of-records)))))
+
+(define employee-list '((1 Jhon) (2 Bruce) (3 Craig) (4 Morgan) (5 Roger) (6 Scott) (7 Daniel) ))
+
+(list->tree employee-list)
+
+(lookup 5 (list->tree employee-list))
+
+;; Exercise 2.67
+
+(define (make-leaf symbol weight)
+  (list 'leaf symbol weight))
+
+(define (leaf? object)
+  (eq? (car object) 'leaf))
+
+(define (symbol-leaf x) (cadr x))
+
+(define (weight-leaf x) (caddr x))
+
+(define (make-code-tree left right)
+  (list left
+        right
+        (append (symbols left) (symbols right))
+        (+ (weight left) (weight right))))
+
+(define (left-branch-huffman tree) (car tree))
+
+(define (right-branch-huffman tree) (cadr tree))
+
+(define (symbols tree)
+  (if (leaf? tree)
+      (list (symbol-leaf tree))
+      (caddr tree)))
+
+(define (weight tree)
+  (if (leaf? tree)
+      (weight-leaf tree)
+      (cadddr tree)))
+
+(define (decode bits tree)
+  (define (decode-1 bits current-branch)
+    (if (null? bits)
+        '()
+        (let ((next-branch
+               (choose-branch (car bits) current-branch)))
+          (if (leaf? next-branch)
+              (cons (symbol-leaf next-branch)
+                    (decode-1 (cdr bits) tree))
+              (decode-1 (cdr bits) next-branch)))))
+  (decode-1 bits tree))
+
+(define (choose-branch bit branch)
+  (cond ((= bit 0) (left-branch-huffman branch))
+        ((= bit 1) (right-branch-huffman branch))
+        (else (error "bad bit -- CHOOSE-BRANCH" bit))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< (weight x) (weight (car set))) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (make-leaf-set pairs)
+  (if (null? pairs)
+      '()
+      (let ((pair (car pairs)))
+        (adjoin-set (make-leaf (car pair)    ; symbol
+                               (cadr pair))  ; frequency
+                    (make-leaf-set (cdr pairs))))))
+
+(define sample-tree
+  (make-code-tree (make-leaf 'A 4)
+                  (make-code-tree
+                   (make-leaf 'B 2)
+                   (make-code-tree (make-leaf 'D 1)
+                                   (make-leaf 'C 1)))))
+
+(define sample-message '(0 1 1 0 0 1 0 1 0 1 1 1 0))
+
+(decode sample-message sample-tree)
+;; => (A D A B B C A)
+
+;; Exercise 2.68
+
+(define (encode message tree)
+  (if (null? message)
+      '()
+      (append (encode-symbol (car message) tree)
+              (encode (cdr message) tree))))
+
+(define (encode-symbol symbol tree)
+  (cond ((leaf? tree) '())
+        ((element-of-set? symbol (symbols (left-branch-huffman tree)))
+         (cons 0 (encode-symbol symbol (left-branch-huffman tree))))
+        ((element-of-set? symbol (symbols (right-branch-huffman tree)))
+         (cons 1 (encode-symbol symbol (right-branch-huffman tree))))
+        (else (error "bad symbol -- ENCODE-SYMBOL" symbol))))
+
+(encode '(A D A B B C A) sample-tree)
+;; => (0 1 1 0 0 1 0 1 0 1 1 1 0)
+
+;; Exercise 2.69
+
+(define (generate-huffman-tree pairs)
+  (successive-merge (make-leaf-set pairs)))
+
+(define (successive-merge leaf-set)
+  (if (null? (cdr leaf-set))
+      (car leaf-set)
+      (let ((new-branch (make-code-tree (car leaf-set) (cadr leaf-set))))
+        (successive-merge (adjoin-set new-branch (cddr leaf-set))))))
+
+(define generated-tree (generate-huffman-tree '((A 4) (B 2) (C 1) (D 1))))
+
+(decode sample-message generated-tree)
+;; => (A D A B B C A)
+
+(encode '(A D A B B C A) generated-tree)
+;; => (0 1 1 0 0 1 0 1 0 1 1 1 0)
+
+;; Exercise 2.70
+
+(define song-alphabet '((a 2) (boom 1) (get 2) (job 2) (na 16) (sha 3) (yip 9) (wah 1)))
+
+(define song-tree (generate-huffman-tree song-alphabet))
+
+(define song-message '(get a job sha na na na na na na na na get a job sha na na na na na na na na wah yip yip yip yip yip yip yip yip yip sha boom))
+
+(define encoded-song (encode song-message song-tree))
+
+encoded-song
+
+;; variable-length codes
+(length encoded-song)
+;; => 84
+
+;; fixed-length codes
+(* 3 (length song-message))
+;; => 108
+
+;; Exercise 2.71
+
+;; the tree for n=5
+;;
+;;   /\
+;;  /  \ 15 (8 + 7)
+;; 16  /\  
+;;    /  \ 7 (4 + 3)
+;;   8   /\
+;;      /  \ 3 (2 + 1)
+;;     4   /\
+;;        /  \
+;;       2    1
+
+;; the tree for n=10 look the same
+
+;; to encode the most frequent symbol we need 1 bit (depth of the symbol)
+;; to encode the least frequent symbol we need n-1 bit (once again depth of the symbol)
+
+;; Exercise 2.71
+
+;; the order of growth in case of encoding the most frequent symbol is O(n)
+;; the order of growth in case of encoding the least frequent symbol is O(n^2)
